56218d95d250bf70c6ce8f85c1e827da7831c728
t3sn <t3sntech@gmail.com>
2016-07-04 06:29:34 +0530

    Corrected filerev task removal

--- Gruntfile.js
+++ Gruntfile.js
@@ -256,16 +256,16 @@ module.exports = function (grunt) {
     },
 
     // Renames files for browser caching purposes
-    // filerev: {
-    //   dist: {
-    //     src: [
-    //       '<%= yeoman.dist %>/scripts/{,*/}*.js',
-    //       '<%= yeoman.dist %>/styles/{,*/}*.css',
-    //       '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-    //       '<%= yeoman.dist %>/styles/fonts/*'
-    //     ]
-    //   }
-    // },
+    filerev: {
+      dist: {
+        src: [
+          '<%= yeoman.dist %>/scripts/{,*/}*.js',
+          '<%= yeoman.dist %>/styles/{,*/}*.css',
+          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          '<%= yeoman.dist %>/styles/fonts/*'
+        ]
+      }
+    },
 
     // Reads HTML for usemin blocks to enable smart builds that automatically
     // concat, minify and revision files. Creates configurations in memory so
@@ -503,7 +503,7 @@ module.exports = function (grunt) {
     //'cdnify',
     'cssmin',
     'uglify',
-    'filerev',
+    //'filerev',
     'usemin',
     //'htmlmin'
   ]);
